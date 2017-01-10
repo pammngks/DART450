e449f964f03871a0c3fe24c57ec8177ab3f1ad9f
Pam Menegakis <pam_men@live.concordia.ca>
2017-01-10 17:21:54 -0500

    Changed text

--- exercises/exercise01/js/script.js
+++ exercises/exercise01/js/script.js
@@ -1,13 +1,9 @@
 /*
 
-DART450
 Week 01 January 10 2017
 Text Change
 
 */
-
-// We use "document ready" so that our code only runs once the whole
-// webpage is loaded and ready.
 
 $(document).ready(function() {
   $('#text').hover(function () {
