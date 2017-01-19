b45f6a9266c7602821f39868ee852f8cd5575ff5
Pam Menegakis <pam_men@live.concordia.ca>
2017-01-19 12:36:05 -0500

    Changed variables

--- exercises/exercise 02/js/script.js
+++ exercises/exercise 02/js/script.js
@@ -6,14 +6,21 @@ Narrative and Times
 */
 
 $(document).ready(function() {
-  var date = new Date();
-  var greeting = "Good";
-    if (now.getHours() < 17) {
-      greeting += " evening.";
-    }
-    else {
-      greeting += " day.";
-    }
+  var date = new Date().getHours();
+  var greeting;
+  var morning = ('Good morning');
+  var afternoon = ('Good afternoon');
+  var evening = ('Good evening');
+
+  if (date >= 0 && date < 12) {
+    greeting = morning;
+  }
+  else if (date >= 12 && date < 17) {
+    greeting = afternoon;
+  }
+  else if (date >= 17 && date < 24) {
+    greeting = evening;
+  }
 
 
 $('#today').text(greeting);
