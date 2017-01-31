5acc5aa251a15c40bcd4f973ed3de2a046eefef1
Pam Menegakis <pam_men@live.concordia.ca>
2017-01-31 14:02:14 -0500

    Added images

--- exercises/exercise03/js/script.js
+++ exercises/exercise03/js/script.js
@@ -4,14 +4,69 @@ It's Supposed To Look Like Shit
 
 */
 
-$(document).ready(function() {
+// The total number of images we'll add to the page
+// is going to be a random integer between 0 and 99
+const TOTAL_IMAGES = randomIntegerInRange(0,10);
+const AVAILABLE_IMAGES = 4;
+const HIDE_DELAY = 2000;
 
-  const TOTAL_DIVS = 10;
-  const INTERVAL = 100;
-  setInterval(function () {
-    for (var i = 0; i < TOTAL_DIVS; i++) {
-      var div = $('<div id="div' + i + '"></div>')
-      $('body').append(div);
-    }
-  },INTERVAL);
+
+$(document).ready(function() {
+  var r = Math.random();
+  if (r < 0.25) {
+  }
+  else if (r < 0.5) {
+      setTimeout(function () {
+      $('body').hide();
+    },HIDE_DELAY);
+  }
+  else {
+    messUpPage();
+  }
 });
+
+
+function messUpPage() {
+  $("#title").click(titleClicked);
+  $('.line').each(function () {
+    randomRotate($(this),-45,45);
+  });
+
+  for (var i = 0; i < TOTAL_IMAGES; i++) {
+    var img = createRandomImage();
+    $('body').append(img);
+  }
+}
+
+function titleClicked() {
+  randomRotate($(this),-20,20);
+}
+
+function createRandomImage() {
+
+  var imageNumber = randomIntegerInRange(0,AVAILABLE_IMAGES);
+  var imageSource = "images/image" + imageNumber + ".jpg";
+  var imageX = randomIntegerInRange(0,$(document).width());
+  var imageY = randomIntegerInRange(0,$(document).height());
+  var img = $('<img class="image" src="' + imageSource + '">');
+
+  img.css({
+    top: imageY + 'px',
+    left: imageX + 'px'
+  });
+
+  randomRotate(img,-30,30);
+  return img;
+}
+
+function randomRotate(element,min,max) {
+  rotation = randomIntegerInRange(min,max);
+
+  element.css({
+    "transform": "rotateZ(" + rotation + "deg)"
+  });
+}
+
+function randomIntegerInRange(min,max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
