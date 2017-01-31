f5c49609ebe0abc0f51ea3c505b6e779da363ceb
Pam Menegakis <pam_men@live.concordia.ca>
2017-01-31 13:12:37 -0500

    Updates files

--- exercises/exercise03/js/script.js
+++ exercises/exercise03/js/script.js
@@ -1,17 +1,16 @@
 /*
 
-January 31 2017
 It's Supposed To Look Like Shit
 
 */
 
-$(document).ready(function(){
-  const TOTAL_DIVS = 10;
-  const INTERVAL = 1000;
+$(document).ready(function() {
 
-  setInterval(function(){
-    for (var i=0; i < TOTAL_DIVS; i++) {
-      var div = $('<div id="circle' + i + '"></div>')
+  const TOTAL_DIVS = 10;
+  const INTERVAL = 100;
+  setInterval(function () {
+    for (var i = 0; i < TOTAL_DIVS; i++) {
+      var div = $('<div id="div' + i + '"></div>')
       $('body').append(div);
     }
   },INTERVAL);
