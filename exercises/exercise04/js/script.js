5b2e6565a7945bc9d48a79dd3797e93e363c9cb7
Pam Menegakis <pammenegakis@Pams-MacBook-Pro.local>
2017-02-07 00:46:33 -0500

    Added comments in js

--- exercises/exercise04/js/script.js
+++ exercises/exercise04/js/script.js
@@ -11,15 +11,15 @@ var myArray = ["world", "universe", "person"];
 var myIndex = 1;
 var print = document.getElementById('print');
 
-print.innerHTML = myArray[0];
+print.innerHTML = myArray[0]; // display first value of array
 
-$('#click').click(function (){
+$('#click').click(function (){ // trigger nextElement function on click
   nextElement();
 })
 
 function nextElement() {
    print.innerHTML = myArray[myIndex];
-   myIndex = (myIndex+1)%(myArray.length);
+   myIndex = (myIndex+1)%(myArray.length); // remaining array values
 };
 
 });
