ceca62aa54b960737e01d8e326ea81bffebe37ba
Pam Menegakis <pammenegakis@Pams-MacBook-Pro.local>
2017-02-07 00:01:32 -0500

    Added comments

--- exercises/exercise04/js/script.js
+++ exercises/exercise04/js/script.js
@@ -9,10 +9,14 @@ document.addEventListener( "click", function toggleWords (event) {
 
     var target = event.target, values = [], placed;
 
+// If clicked element has multiple values
     if ( target.hasAttribute("data-values") ) {
+// Split values into array
         values = target.getAttribute("data-values").split(",");
+// Find location
         placed = values.indexOf(target.textContent);
+// Change text to next value in array
         target.textContent = values[++placed % values.length];
       }
-      
+
   });
