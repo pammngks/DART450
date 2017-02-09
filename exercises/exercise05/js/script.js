459c51ff37333f26daed9c08ca3e5e58f96c8fa4
Pam Menegakis <pammenegakis@Pams-MacBook-Pro.local>
2017-02-09 17:25:43 -0500

    added jQuery

--- exercises/exercise05/js/script.js
+++ exercises/exercise05/js/script.js
@@ -5,6 +5,26 @@
 */
 
 $(document).ready(function() {
+  $('#button').click (function(){
+    var toAdd = $('input[name=listItem]').val();
+    $('ol').append('<li>' + toAdd + '</li>');
+  });
+
+  $(document).on('dblclick', 'li', function(){
+    $(this).toggleClass('strike').fadeOut('slow');
+  });
+
+  $('input').focus(function(){
+    $(this).val('');
+  });
+
+  $('ol').sortable();
+}
+);
+
+
+
+
 
 // $(document).click(function() { // on page click
 //   responsiveVoice.speak("Just get started on it already","US English Male");
