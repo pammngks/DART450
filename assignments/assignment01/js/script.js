25ebd2e4af44a28993b6168ae7a0222afd1317b2
Pam Menegakis <pammenegakis@Pams-MacBook-Pro.local>
2017-02-17 15:19:37 -0500

    Simplified date and time
    Added variables to return hours and minutes. Added conditionals to
    return suffix AM or PM.

--- assignments/assignment01/js/script.js
+++ assignments/assignment01/js/script.js
@@ -8,9 +8,23 @@ February 2017
 
   // Display date and time
 $(document).ready(function(){
-  var currentDate = new Date (); // gets current date
-  var monthNames = ["Jan", "Feb", "Mar", "Apr", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-  var dayNames = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri"];
 
-  // document.getElementById('date').innerHTML="text";
+  var currentDate = new Date (); // returns current date
+  var hours = currentDate.getHours(); // returns the hour (0-23)
+  var minutes = currentDate.getMinutes(); // returns the minutes (0-59)
+
+    if (minutes < 10) {
+     minutes = "0" + minutes; // add 0 to minutes if less than 10
+    }
+
+  var suffix = "AM";
+    if (hours >= 12) {
+      suffix = "PM"; // if hour is greater or equal to 12, display PM
+      hours = hours - 12; // display time in 12 hour format
+    }
+    if (hours == 0) {
+     hours = 12;
+    }
+
+  document.getElementById('date').innerHTML=currentDate.toDateString() + " " + hours + ":" + minutes + " " + suffix; // display above variables in #date
 });
