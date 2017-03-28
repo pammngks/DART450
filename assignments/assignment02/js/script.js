27a8d9ac9658466e76812f40c84c7595e1763575
Pam Menegakis <pammenegakis@pams-mbp.wireless.concordia.ca>
2017-03-28 15:20:30 -0400

    Created alert if name input is blank

--- assignments/assignment02/js/script.js
+++ assignments/assignment02/js/script.js
@@ -7,13 +7,17 @@ Pam Menegakis
 
 $(document).ready(function() {
 
-$('.enter').click(function(){
+$('.enter').click(function (){
   if (!$('#check').is(':checked')){
     alert('Please confirm that you are not a robot.');
     return false;
   }
   else {
-    alert('Yeah you are.');
+    window.location= "./web/main.html";
+  }
+
+  if ($('#name').val().length === 0) {
+    alert("Don't you have a name?");
   }
 });
 
