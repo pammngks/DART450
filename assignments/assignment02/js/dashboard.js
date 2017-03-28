77448856521aeb51f86e55519ef582482f7cd6e0
Pam Menegakis <pammenegakis@pams-mbp.wireless.concordia.ca>
2017-03-28 16:43:37 -0400

    Added page count on click

--- assignments/assignment02/js/dashboard.js
+++ assignments/assignment02/js/dashboard.js
@@ -16,6 +16,14 @@ $('#add').click(function(){
   $('.counter').text(counter);
 });
 
+var pageCount = 0;
+$(document).click(function(){
+  pageCount++;
+  if (pageCount > 5){
+    document.body.style.background = 'black';
+  }
+});
+
 
 
 });
