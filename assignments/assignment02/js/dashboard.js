1216f55873a4c65fcc88acb0b75e6d548af1f438
Pam Menegakis <pammenegakis@Pams-MacBook-Pro.local>
2017-04-03 18:37:33 -0400

    Created userGreeting function

--- assignments/assignment02/js/dashboard.js
+++ assignments/assignment02/js/dashboard.js
@@ -7,6 +7,9 @@ Pam Menegakis
 
 $(document).ready(function() {
 
+// Displays a greeting to the user based upon the current time
+userGreeting();
+
   var counter = 0;
 
 $('#add').click(function(){
@@ -29,7 +32,31 @@ $(document).click(function(){
   }
 });
 
+function  userGreeting() {
+  var today = new Date(); // Create new date object
+  var hourNow = today.getHours(); // Find the current hour
+  var greeting;
 
+// Displays the appropriate greeting based on the current time
+  if (hourNow > 18) {
+    greeting = 'Good evening';
+  }
+
+  else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+  }
+
+  else if (hourNow > 0) {
+    greeting = 'Good morning';
+  }
+
+  else {
+    greeting = 'Welcome';
+  }
+
+  document.getElementById('header').innerHTML = greeting;
+
+}
 
 
 });
