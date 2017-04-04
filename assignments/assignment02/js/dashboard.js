ae59c663bc2432d0558c7c9c4cbc5bb4ee828600
Pam Menegakis <pammenegakis@pams-mbp.wireless.concordia.ca>
2017-04-04 16:50:04 -0400

    Added comments

--- assignments/assignment02/js/dashboard.js
+++ assignments/assignment02/js/dashboard.js
@@ -1,6 +1,8 @@
 /*
 
-Script for dashboard page.
+Script for dashboard page -
+At a first glance, the page will look simple and functional to the user. As they keep using it, the page slowly destructs by making elements harder to use. The user is invited to keep exploring and making use of the website, but at a certain point it will black out making it completely useless.
+
 Pam Menegakis
 
 */
@@ -15,35 +17,45 @@ $.getJSON('../data/data.json', gotData);
 
 });
 
+// FUNCTION: Keep count of the tasks a user adds to the list with a counter
+
+// Set initial count to 0
 var counter = 0;
 
+// Run the function every time the user clicks the add button
   $('#add').click(function(){
+    // Set input value as a variable
     var toAdd = $('input[name=listItem]').val();
-    $('ul').append('<li>' + toAdd + '</li>'); //Adds input to list
+    // Add input value to the list
+    $('ul').append('<li>' + toAdd + '</li>');
+    // Each time a value is added, add one to the counter
     counter++;
+    // Display the counter in the header, followed by text
     $('.counter').text(counter + ' tasks to complete');
 
   });
 
 
-// Function: Keep track of the page clicks in order to destruct elements
+// FUNCTION: Keep track of the page clicks in order to destruct elements
+
+// Set initial count to 0
   var pageCount = 0;
-
+// Run the function every time the user clicks anywhere in the page
   $(document).click(function(){
+    // Each time a click is generated, add it to the count
     pageCount++;
-
+    // Various if statements to render the page 'difficult' to use
     if (pageCount > 30){
+      // Expose a class that will glitch the entire web page with external css styling
       $('body').addClass("glitch");
     }
 
     if (pageCount > 5) {
-
-      $('div').each(function(){ // Go through each div on the page
-
+      // Go through each div on the page
+      $('div').each(function(){
         // Select a random location anywhere within the window
         var x = $(window).width() * Math.random();
         var y = $(window).height() * Math.random();
-
         // Set the current div's CSS to put it at a random location
         $(this).css({
          position: 'absolute',
@@ -55,14 +67,16 @@ var counter = 0;
   });
 
 
-// Function: Display a greeting to the user based upon the current time
+// FUNCTION: Display a greeting to the user based upon the current time
 
 function  userGreeting() {
-  var today = new Date(); // Create new date object
-  var hourNow = today.getHours(); // Find the current hour
+  // Create new date object
+  var today = new Date();
+  // Find the current hour
+  var hourNow = today.getHours();
   var greeting;
 
-// Displays the appropriate greeting based on the current time
+  // Displays the appropriate greeting based on the current time
   if (hourNow > 18) {
     greeting = 'Good evening';
   }
@@ -78,12 +92,13 @@ function  userGreeting() {
   else {
     greeting = 'Welcome';
   }
-
-  document.getElementById('hello').innerHTML = greeting; // Write the greeting in the header
+  // Write the greeting in the header
+  document.getElementById('hello').innerHTML = greeting;
 
 }
 
-// Function: Display a random first name, last name and task count in the leaderboard section
+// FUNCTION: Display a random person and task count in the leaderboard section
+
 function gotData(data) {
   // Get a first name
   var firstName = getRandomElement(data.firstName);
@@ -92,11 +107,12 @@ function gotData(data) {
   // Get a task count
   var taskCount = getRandomElement(data.taskCount);
 
-// Get random elements every time the page loads
+  // Get random elements every time the page loads
   function getRandomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
+  // Display the variables above in the leaderboard
   $('.name').text(firstName + " " + lastName);
   $('.taskCount').text(taskCount);
 }
