db04ca416054d0ae7f7a82b7c4e7015b1d922c1a
Pam Menegakis <pammenegakis@Pams-MacBook-Pro.local>
2017-04-30 20:32:20 -0400

    Created js file for glitch states

---
+++ assignments/assignment02/js/glitch.js
@@ -0,0 +1,48 @@
+/*
+
+Script for glitch states -
+
+Pam Menegakis
+
+*/
+
+// FUNCTION: Keep track of the page clicks in order to destruct elements
+function pageGlitch() {
+  // Set initial count to 0
+  var pageCount = 0;
+  // Run the function every time the user clicks anywhere in the page
+  $(document).click(function(){
+    // Each time a click is generated, add it to the count
+    pageCount++;
+    // Various if statements to render the page 'difficult' to use
+    if (pageCount == 50){
+      // Expose a class that will turn the page black with external css styling
+      $('body').addClass('glitch');
+    }
+
+    if (pageCount == 20){
+        // Expose a class that will shake each div element
+        $('div').addClass('shake');
+    }
+
+    if (pageCount == 22) {
+      $('div').addClass('shake-2');
+    }
+
+    if (pageCount == 24) {
+      // Go through each div on the page
+      $('div').each(function(){
+        // Select a random location anywhere within the window
+        var x = $(window).width() * Math.random();
+        var y = $(window).height() * Math.random();
+        // Set the current div's CSS to put it at a random location
+        $(this).css({
+          position: 'absolute',
+          top: y + 'px',
+          left: x + 'px'});
+          $(this).removeClass('shake');
+        $(this).draggable();
+        });
+      };
+    });
+  };
